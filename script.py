c882e833d7ceba61a822506bf87d8c0ca99e3acd
David Garfinkle <david.garfinkle@mail.mcgill.ca>
2017-10-20 19:27:09 +0900

    testing script

--- script.py
+++ script.py
@@ -5,10 +5,14 @@ import classifier
 
 TRAIN_SET_TEXT_PATH = 'data/train_set_x.csv'
 TRAIN_SET_LABELS_PATH = 'data/train_set_y.csv'
+TEST_SET_TEXT_PATH = 'data/test_set_x.csv'
 
-data = data_parser.DataModel(TRAIN_SET_TEXT_PATH, TRAIN_SET_LABELS_PATH)
+training_data = data_parser.DataModel(TRAIN_SET_TEXT_PATH, TRAIN_SET_LABELS_PATH)
+#testing_data = data_parser.DataModel(TEST_SET_TEXT_PATH)
 
-my_classifier = classifier.NaiveBayesianClassifier(data)
-my_classifier.train()
+my_classifier = classifier.NaiveBayesianClassifier(training_data)
+
+
+
 
 
